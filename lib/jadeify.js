domenic/simple-jadeify

Read jadeify options from package.json

lib/jadeify.js
@@ -1,7 +1,29 @@
 "use strict";
 
+var path = require("path");
 var through = require("through");
 var jade = require("jade");
+var findParent = require("find-parent-dir");
+
+
+function readPackage (pathName, cb) {
+    findParent(pathName, "package.json", function (err, dir) {
+        if (err) {
+            return cb(err);
+        }
+        if (!dir) {
+            return cb(null, null);
+        }
+        var fileName = path.join(dir, "package.json");
+        var pkg = null;
+        try {
+            pkg = require(fileName);
+        } catch (e) {
+            return cb(e);
+        }
+        cb(null, pkg);
+    });
+}
 
 module.exports = function (fileName) {
     if (!/\.jade$/i.test(fileName)) {
@@ -15,27 +37,32 @@ module.exports = function (fileName) {
             inputString += chunk;
         },
         function () {
-            var opts = { filename: fileName, compileDebug: false };
+            var self = this;
+            readPackage(process.cwd(), function (err, pkg) {
+                var opts = pkg && pkg.jadeify || {};
+                opts.filename = fileName;
+                opts.compileDebug = false;
 
-            var templateFunction;
+                var templateFunction;
 
-            try {
-                if (jade.compileClient) {
-                    templateFunction = jade.compileClient(inputString, opts);
-                } else {
-                    opts.client = true;
-                    templateFunction = jade.compile(inputString, opts);
+                try {
+                    if (jade.compileClient) {
+                        templateFunction = jade.compileClient(inputString, opts);
+                    } else {
+                        opts.client = true;
+                        templateFunction = jade.compile(inputString, opts);
+                    }
+                } catch (e) {
+                    self.emit("error", e);
+                    return;
                 }
-            } catch (e) {
-                this.emit("error", e);
-                return;
-            }
 
-            var moduleBody = "var jade = require(\"jade/runtime\");\n\n" +
-                             "module.exports = " + templateFunction.toString() + ";";
+                var moduleBody = "var jade = require(\"jade/runtime\");\n\n" +
+                                 "module.exports = " + templateFunction.toString() + ";";
 
-            this.queue(moduleBody);
-            this.queue(null);
+                self.queue(moduleBody);
+                self.queue(null);
+            });
         }
     );
 };

domenic/jadeify

lib/jadeify.js
@@ -1,7 +1,9 @@
 "use strict";
 
+var path = require("path");
 var through = require("through");
 var jade = require("jade");
+var findParent = require("find-parent-dir");
 
 module.exports = function (fileName) {
     if (!/\.jade$/i.test(fileName)) {
@@ -15,27 +17,51 @@ module.exports = function (fileName) {
             inputString += chunk;
         },
         function () {
-            var opts = { filename: fileName, compileDebug: false };
+            var self = this;
+            readPackage(process.cwd(), function (err, pkg) {
+                var opts = pkg && pkg.jadeify || {};
+                opts.filename = fileName;
+                opts.compileDebug = false;
 
-            var templateFunction;
+                var templateFunction;
 
-            try {
-                if (jade.compileClient) {
-                    templateFunction = jade.compileClient(inputString, opts);
-                } else {
-                    opts.client = true;
-                    templateFunction = jade.compile(inputString, opts);
+                try {
+                    if (jade.compileClient) {
+                        templateFunction = jade.compileClient(inputString, opts);
+                    } else {
+                        opts.client = true;
+                        templateFunction = jade.compile(inputString, opts);
+                    }
+                } catch (e) {
+                    self.emit("error", e);
+                    return;
                 }
-            } catch (e) {
-                this.emit("error", e);
-                return;
-            }
 
-            var moduleBody = "var jade = require(\"jade/runtime\");\n\n" +
-                             "module.exports = " + templateFunction.toString() + ";";
+                var moduleBody = "var jade = require(\"jade/runtime\");\n\n" +
+                                 "module.exports = " + templateFunction.toString() + ";";
 
-            this.queue(moduleBody);
-            this.queue(null);
+                self.queue(moduleBody);
+                self.queue(null);
+            });
         }
     );
 };
+
+function readPackage(pathName, cb) {
+    findParent(pathName, "package.json", function (err, dir) {
+        if (err) {
+            return cb(err);
+        }
+        if (!dir) {
+            return cb(null, null);
+        }
+        var fileName = path.resolve(dir, "package.json");
+        var pkg = null;
+        try {
+            pkg = require(fileName);
+        } catch (e) {
+            return cb(e);
+        }
+        cb(null, pkg);
+    });
+}